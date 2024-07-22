4193cc191eda1924d34c6addbcf2c52dc7c2f093
Monand <monandszy@tuta.io>
2024-07-22 13:14:07 +0200

    aggregate

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,14 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.modulith.Modulithic;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
 import java.time.Clock;
 import java.time.ZoneOffset;
